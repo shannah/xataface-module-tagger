2c2c4bef83fa57080bda7395665f7ef1ee35c36b
Steve Hannah <steve@weblite.ca>
2014-05-26 10:45:20 -0700

    Fixed tag cloud sizes to be increase with frequency properly. http://bugs.weblite.ca/view.php?id=1496

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -8,7 +8,7 @@
 		$('.xf-tagcloud li').each(function(){
 		
 			var freq = parseInt($(this).attr('data-xf-frequency'));
-			$(this).children().css('font-size', (freq / 10 < 1) ? freq / 10 + 1 + "em": (freq / 10 > 2) ? "2em" : freq / 10 + "em");
+			$(this).children().css('font-size', (freq / 10 < 1) ? (freq / 10 + 0.5) + "em": (freq / 10 > 2) ? "2.5em" : (freq / 10 + 0.5) + "em");
 		
 		});
 		
